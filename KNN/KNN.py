0f96d2143cfb581d398b53cb21eb58d4a170b46a
watermelon-lee <1090441337@qq.com>
2019-11-24 19:22:01 +0800

    come on

---
+++ KNN/KNN.py
@@ -0,0 +1,142 @@
+"""
+@File    : KNN.py
+@Time    : 2019-10-22 12:27
+@Author  : 李浩然
+@Software: PyCharm
+"""
+
+# mnist_train:60000
+# mnist_test:500
+
+# acc: 0.956
+# time: 1032s
+
+
+
+import pandas as pd
+import numpy as np
+import time
+from collections import Counter
+
+
+
+
+def loadData(fileName):
+    #从文件中读取数据
+    data=pd.read_csv(fileName,header=None)
+    # 将数据从dataframe转化为ndarray
+    data=data.values
+    #数据第一行为分类结果
+    y_label=data[:,0]
+
+
+    # 将数据转化为矩阵（matrix）
+    x_label=np.mat(data[:,1:])
+
+    #数据归一化，返回数据
+    # y_label为np.ndarray,x_label为np.matrix
+    return x_label/255,y_label
+
+
+#计算两个点的距离
+# 使用欧式距离 dis=sqrt(square（x1-x2）)
+# x1，x2均为numpy.matrix ,1x784 行向量
+def caculDistance(x1,x2):
+    return np.sqrt(np.sum(np.square(x1-x2)))
+
+
+# X_train为训练集中的数据，y_label为对应的分类标签
+# x为所给的带分类数据
+# k是我们判断最近k个点的k值
+def findCluster(X_train,y_train,x,k):
+    distances=[] #用于保存距离
+    for x_train in X_train:
+        #计算样本与训练集样本的距离
+        dis=caculDistance(x_train,x)
+        distances.append(dis)
+    # 遍历完所有训练集数据之后,我们就可以进行统计分类
+    # 可以知道distances的索引与y_label的索引一一对应
+    # 如distances[1]=7.43，y_label[1]=1
+    # 则x与分类结果 1 的距离为7.43
+
+    #下面我们需要找到distances中最小的k个元素，并且返回他们的索引就可以了
+    # argsort函数返回的是数组值从小到大的索引值
+    # 将distances变为np.array，然后对索引排序，取前k个
+    minK=np.argsort(np.array(distances))[:k]
+
+    # 将对应分类结果添加到result中
+    result=[]
+    for i in range(k):
+        result.append(y_train[minK[i]])
+
+    #a=[1,2,3,4,4,4,4,4,4,4,1,1,1,23,2,2,2,3,1]
+    # b=Counter(a)
+    # b.most_common(1)
+    # out:[(4,7)] 4是元素，7是出现次数
+    #下面我们就可以在result中找出出现最多的值作为分类结果了
+
+    belonging=Counter(result).most_common(1)[0][0]
+
+    return belonging
+
+def test(X_train,y_train,X_test,y_test,k):
+    # 只测试500个
+
+    acc_num=0# 正确个数
+    acc=0 #正确率
+    for i in range(500):
+        cluster=findCluster(X_train,y_train,X_test[i],k)
+        if cluster==y_test[i]:
+            acc_num+=1
+        print(f'find {i}th data cluster:cluster_pred={cluster},cluster={y_test[i]}')
+        print('now_acc=',acc_num/(i+1))
+
+
+
+
+
+if __name__=='__main__':
+    # 获取当前时间
+    start = time.time()
+
+    #读取训练文件
+    X_train,y_train=loadData('../Mnist/mnist_train.csv')
+
+    # 读取测试文件
+    X_test,y_test=loadData('../Mnist/mnist_test.csv')
+
+    test(X_train,y_train,X_test,y_test,k=20)
+
+    #获取结束时间
+    end=time.time()
+
+    print('run time:',end-start)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ NaiveBayes/NaiveBayes.py
@@ -0,0 +1,134 @@
+"""
+@File    : NaiveBayes.py
+@Time    : 2019-10-22 16:24
+@Author  : 李浩然
+@Software: PyCharm
+"""
+
+# mnist_train:60000
+# mnist_test:10000
+# acc: 0.8427
+# time: 129s
+
+
+import pandas as pd
+import numpy as np
+import time
+from collections import Counter
+
+
+
+
+def loadData(fileName):
+    #从文件中读取数据
+    data=pd.read_csv(fileName,header=None)
+    # 将数据从dataframe转化为ndarray
+    data=data.values
+    #数据第一行为分类结果
+    y_label=data[:,0]
+    x_label=data[:,1:]
+
+    #数据二值化，返回数据
+    #因为xi的取值范围为0-255，则计算p(X=xi\Y=y)的时候可能性过多，计算过于繁杂
+    # 所以进行二值化
+    # y_label为np.ndarray,x_label为np.ndarray
+
+    x_label[x_label<128]=0
+    x_label[x_label>=128]=1
+
+    return x_label,y_label
+
+
+#计算先验概率以及条件概率
+def caculPrior(X_train,y_train):
+    y_class=10 # 分类的10种结果
+    feature_x=784 # x特征有784列
+
+    Iy=np.array([0]*y_class)  # 出现次数
+    Py=np.array([0]*y_class)  # P(Y=y)
+    Ixy=np.zeros((y_class,feature_x,2)) #出现次数
+    Pxy=np.zeros((y_class,feature_x,2))  # P(X=x|Y=y)
+
+    # ！！！ 我们这里是有784个特征，784个0-1的数字相乘，会得到一个非常小的数
+    # 很有可能造成下溢出。所以我们将其进行log处理
+    # 可以知道log（x）随着x单增。px越大。
+
+    #计算先验概率P(Y=y)
+    for i in range(len(y_train)):
+        # 先计算出现次数，之后在➗总数
+        Iy[y_train[i]]+=1
+
+    # P(Y=yi)=I(Y=yi)/N
+    Py=np.log(Iy/len(y_train))
+
+    # 计算条件概率
+    # P(X=xi|Y=yi)=P(X=xi,Y=yi)/P(Y=yi)
+    # 先计算出出现次数
+    for i in range(len(X_train)):
+        print(i)
+
+        for j in range(len(X_train[i])):
+            # Ixy=np.zeros((y_class,feature_x,2))
+            # 所以Ixy是一个三位数组（10x784x2）
+
+            Ixy[y_train[i]][j][X_train[i][j]]+=1
+
+    print('***********************')
+    # ➗总次数作为概率
+    # P(X=xi|Y=yi)=I(Y=yi,X=xi)/I(Y=yi)
+    for i in range(y_class):
+        print(i)
+        for j in range(feature_x):
+            # 加上普拉斯平滑，避免出现0的情况
+            # P(X=xi|Y=yi)=(I(Y=yi,X=xi)+lambda)/(I(Y=yi)+lambda*Sj)
+            # 此处Sj=2，取值为0，1。lambda取1
+            Pxy[i][j][0]=np.log((Ixy[i][j][0]+1)/(Iy[i]+2))
+            Pxy[i][j][1]=np.log((Ixy[i][j][1]+1)/(Iy[i]+2))
+
+    return Py,Pxy
+
+
+def naiveBayes(Py,Pxy,x_test):
+    y_class = 10  # 分类的10种结果
+    feature_x = 784  # x特征有784列
+
+    P=[0 for _ in range(y_class)]# 用来存放Y=0-9的可能性
+
+    # x_test 1x784
+    for i in range(y_class):
+        for j in range(feature_x):
+            # y=i的可能性
+            P[i]+=Pxy[i][j][x_test[j]]
+        # 记得要加上Py[i] ！！！
+        P[i]=P[i]+Py[i]
+    return P.index(max(P))
+
+def test(X_train, y_train, X_test, y_test):
+    Py,Pxy=caculPrior(X_train,y_train)
+
+    acc_num=0
+    acc=0
+    for i in range(len(X_test)):
+        y_pred=naiveBayes(Py,Pxy,X_test[i])
+        if y_pred==y_test[i]:
+            acc_num+=1
+        print(f'find {i}th data cluster:y_pred={y_pred},y={y_test[i]}')
+        print('now_acc=', acc_num / (i + 1))
+
+if __name__=="__main__":
+    # 获取当前时间
+    start = time.time()
+
+    # 读取训练文件
+    X_train, y_train = loadData('../Mnist/mnist_train.csv')
+
+    # 读取测试文件
+    X_test, y_test = loadData('../Mnist/mnist_test.csv')
+
+    test(X_train, y_train, X_test, y_test,)
+
+    # 获取结束时间
+    end = time.time()
+
+    print('run time:', end - start)
+
